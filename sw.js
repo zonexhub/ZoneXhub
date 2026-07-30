d7d43f3774eda394b9a5c23d9ec75facfc40c38e
zonexhub <ajid57732@gmail.com>
2026-07-30 15:08:13 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'zonexhub-v1';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
